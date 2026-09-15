476dd924afa8946edaa69f7502f95d8db4ee45dc
luke8086 <55237178+luke8086@users.noreply.github.com>
2026-09-15 19:33:35 +0200

    Apps: Remove Calc app

--- include/p_apps.h
+++ include/p_apps.h
@@ -2,8 +2,6 @@
 extern app_st app_2048;
 /* apps/bjack.c */
 extern app_st app_blackjack;
-/* apps/calc.c */
-extern app_st app_calc;
 /* apps/calendar.c */
 extern app_st app_calendar;
 /* apps/clock.c */
